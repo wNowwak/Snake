390c769f23fb8d978e4ee6a3e85d228b7d2ede40
Wojtek <nowwak.wojtek@gmail.com>
2019-06-12 14:11:10 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,27 @@ import java.awt.*;
 
 
 public class Main {
-    private JPanel panel;
+//    private JPanel panel;
     private JFrame frame;
 
     public static final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+    private static final int framewidth = (int) (0.8*(dimension.width)) - ((int) (0.8*(dimension.width))%25)+13;
+    public static final int frameheight =(int) (0.8*(dimension.height)) - ((int) (0.8*(dimension.height))%25);
     public Main(){
         Rozgrywka rozgrywka = new Rozgrywka();
         frame = new JFrame("SNAKE");
-        frame.setBounds(100,100, (int) (0.8*(dimension.width)), (int) (0.8*dimension.height));
+        frame.setBounds(100,100, framewidth, frameheight);
         frame.getContentPane().setBackground( new Color(125,160,54));
         frame.add(rozgrywka);
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
 
-
+        System.out.println(framewidth+ "\n" + frameheight);
     }
 
     public static void main(String[] args) {
-        System.out.println(dimension);
-        Main snake = new Main();
+       Main snake = new Main();
+
     }
 }
--- src/Rozgrywka.java
+++ src/Rozgrywka.java
@@ -4,22 +4,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
 
     public static final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-    private int screenHeight = (int) (dimension.height*0.8);
-    private int screenWidth = (int) (dimension.width*0.8);
+    private static final int framewidth = (int) (0.8*(dimension.width)) - ((int) (0.8*(dimension.width))%25);
+    public static final int frameheight =(int) (0.8*(dimension.height)) - ((int) (0.8*(dimension.height))%25);
     private int[] dlugoscWezaX = new int[500];
     private int[] dlugoscWezaY = new int[500];
     private int dlugoscSnake = 3;
     private int ruch = 0;
 
+    private Random randomFoodPoss = new Random( );
+    private int foodXpom = randomFoodPoss.nextInt(1000) ;
+    private int foodYpom = randomFoodPoss.nextInt(500) ;
+    private int foodX = foodXpom- (foodXpom%25)+1;
+    private int foodY = foodYpom - (foodYpom%25)+1;
     private ImageIcon head_down;
     private ImageIcon head_up;
     private ImageIcon head_left;
     private ImageIcon head_right;
     private ImageIcon body;
+    private ImageIcon wall;
     private ImageIcon food;
     private Timer timer;
 
@@ -30,27 +37,44 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
     private boolean left = false;
 
     public Rozgrywka(){
+        System.out.println(foodX);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        timer = new Timer(200,this);
+        timer = new Timer(100,this);
         timer.start();
+        if (gameOver == true)
+            timer.stop();
     }
 
-    public void paint(Graphics graphics){
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+        System.out.println(dlugoscWezaX[0]+"\t"+dlugoscWezaY[0]);
+        food = new ImageIcon("img/apple.png");
+        food.paintIcon(this,graphics,foodX,foodY);
 
-        graphics.setColor(Color.BLACK);
-        graphics.drawRect(0,0,screenWidth,screenHeight);
-//            graphics.setColor(Color.WHITE);
+        if (foodX == dlugoscWezaX[0] && foodY == dlugoscWezaY[0]){
+            dlugoscSnake++;
+            foodXpom = randomFoodPoss.nextInt(1000) ;
+            foodYpom = randomFoodPoss.nextInt(500) ;
+            foodX = foodXpom- (foodXpom%25)+1;
+            foodY = foodYpom - (foodYpom%25)+1;
+        }
+
+        graphics.setColor(Color.RED);
+//        System.out.println(frameheight);
+        graphics.drawRect(0,0,framewidth-17,frameheight-40);
+
+////            graphics.setColor(Color.WHITE);
         if(ruch == 0 ){
 
-            dlugoscWezaY [0]     = 50;
-            dlugoscWezaY [1] = 25;
-            dlugoscWezaY [2] = 0;
+            dlugoscWezaY [0]     = 51;
+            dlugoscWezaY [1] = 26;
+            dlugoscWezaY [2] = 1;
 
-            dlugoscWezaX [0] = 10;
-            dlugoscWezaX [1] = 10;
-            dlugoscWezaX [2] = 10;
+            dlugoscWezaX [0] = 1;
+            dlugoscWezaX [1] = 1;
+            dlugoscWezaX [2] = 1;
 
         }
 
@@ -85,10 +109,7 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
             }
 
         }
-//        head_up = new ImageIcon("img/head_up.png");
-//        head_right = new ImageIcon("img/head_right.png");
-//        head_left = new ImageIcon("img/head_left.png");
-        graphics.dispose();
+
     }
 
     @Override
@@ -97,6 +118,7 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
         timer.start();
 
         if (right){
+
             for (int i =dlugoscSnake-1; i>=0 ;i--){
                 dlugoscWezaY[i+1]= dlugoscWezaY[i];
             }
@@ -107,8 +129,8 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
                 else {
                     dlugoscWezaX[i] = dlugoscWezaX[i-1];
                 }
-                if( dlugoscWezaX[i]>screenWidth){
-                    dlugoscWezaX[i]=0;
+                if( dlugoscWezaX[i] == 1501){
+                   gameOver = true;
                 }
             }
             repaint();
@@ -124,8 +146,8 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
                 else {
                     dlugoscWezaX[i] = dlugoscWezaX[i-1];
                 }
-                if( dlugoscWezaX[i]<0){
-                    dlugoscWezaX[i]=screenWidth;
+                if( dlugoscWezaX[i]== 1){
+                    gameOver = true;
                 }
             }
             repaint();
@@ -141,8 +163,8 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
                 else {
                     dlugoscWezaY[i] = dlugoscWezaY[i-1];
                 }
-                if( dlugoscWezaY[i]<0){
-                    dlugoscWezaY[i]=screenHeight;
+                if( dlugoscWezaY[i]<26){
+                    gameOver=true;
                 }
             }
             repaint();
@@ -158,11 +180,14 @@ public class Rozgrywka extends JPanel implements KeyListener, ActionListener {
                 else {
                     dlugoscWezaY[i] = dlugoscWezaY[i-1];
                 }
-                if( dlugoscWezaY[i]>screenHeight){
-                    dlugoscWezaY[i]=0;
+                if( dlugoscWezaY[i]>frameheight-40){
+                   gameOver = true;
                 }
             }
             repaint();
+        }
+        if (gameOver){
+            timer.stop();
         }
 
     }
